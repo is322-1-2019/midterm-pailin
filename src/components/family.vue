10a75d04415a2324b8616e6c8b1bed586f310576
pailin.h3 <jinjuta.l@st.econ.tu.ac.th>
2019-10-02 17:24:24 +0700

    midterm pailin

---
+++ src/components/family.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h5>ข้อมูลบิดา</h5>
+    <form class="form-inline">
+      <label class="mb-2 mr-sm-2">ชื่อ-นามสกุล</label>
+      <input v-model="info.dadName" class="mb-2 mr-sm-2" />
+      <label class="mb-2 mr-sm-2">อาชีพ</label>
+      <input v-model="info.dadCarrer" class="mb-2 mr-sm-2" />
+    </form>
+    <h5>ข้อมูลมารดา<h5>
+    <form class="form-inline">
+      <label class="mb-2 mr-sm-2">ชื่อ-นามสกุล</label>
+      <input v-model="info.momName" class="mb-2 mr-sm-2" />
+      <label class="mb-2 mr-sm-2">อาชีพ</label>
+      <input v-model="info.momCarrer" class="mb-2 mr-sm-2" />
+    </form>
+    <form class="form-inline">
+      <label class="mb-2 mr-sm-2">สถานภาพ</label>
+    </form>
+    <p>{{info}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            info: {
+                dadName: '',
+                momName: '',
+                dadCarrer: '',
+                momCarrer: ''
+            }
+        }
+    }
+}
+</script>
